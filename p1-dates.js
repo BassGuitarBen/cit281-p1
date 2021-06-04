4f889b95b3a99b4bede7f9a5849bc336500ab30a
BassGuitarBen <84551120+BassGuitarBen@users.noreply.github.com>
2021-06-04 16:20:42 -0700

    Create p1-dates.js

---
+++ p1-dates.js
@@ -0,0 +1,28 @@
+/*
+    CIT 281 Project 1
+    Name: Benjamin Wright
+*/
+let weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]; let today = new Date().toString().substring(0,3);  if (today == "Sun") {    console.log(weekdays[0])} else { if (today == "Mon") {    console.log(weekdays[1])} else {if (today == "Tue") {   console.log(weekdays[2])} else {if (today == "Wed") {    console.log(weekdays[3])} else {if (today == "Thu") {    console.log(weekdays[4])} else {if (today == "Fri") {console.log(weekdays[5])} else {if (today == "Sat") {console.log(weekdays[6])} } } } } } } 
+
+/*let weekdays = ["Sunday",
+"Monday",
+"Tuesday",
+"Wednesday",
+"Thursday",
+"Friday",
+"Saturday"]
+let today = new Date().toString().substring(0,3);  if (today == "Sun") {
+    console.log(weekdays[0])
+} else { if (today == "Mon") {
+    console.log(weekdays[1])
+} else {if (today == "Tue") {
+    console.log(weekdays[2])
+} else {if (today == "Wed") {
+    console.log(weekdays[3])
+} else {if (today == "Thu") {
+    console.log(weekdays[4])
+} else {if (today == "Fri") {
+    console.log(weekdays[5])
+} else {if (today == "Sat") {
+    console.log(weekdays[6])
+} } } } } } } */
